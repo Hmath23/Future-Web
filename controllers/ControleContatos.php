05f89428c17ab324174006fc0836623c33a1965d
Matheus Henrique Caraça da Silva <hmatheus23@hotmail.com>
2022-06-01 22:20:59 -0300

    Adequação das funções de incluir e consultar para os botões enviar e listar do formulário

--- controllers/ControleContatos.php
+++ controllers/ControleContatos.php
@@ -12,101 +12,101 @@ $page_key="";
 //Classe que empresta as definições feitas no arquivo SimpleRest
 class ContatosRestHandler extends SimpleRest {
     public function ContatosIncluir() {
-        if(isset($_POST['txtNome'])) {
+            if(isset($_POST['txtNome'])) {
 
-            $nome=$_POST['txtNome'];
-            $endereco=$_POST['txtEndereco'];
-            $fone=$_POST['txtPhone'];
-            $bairro=$_POST['txtBairro'];
-            $cidade=$_POST['txtCidade'];
-            $uf=$_POST['txtUF'];
-            $cep=$_POST['txtCEP'];
-            $email=$_POST['txtMail'];
-            
-            //Declarar a variável código como o último dado inserido na coluna
-            $query = "SELECT codContato from tbcontatos ORDER by codContato desc LIMIT 1";
-            //Instanciar a classe BdTurmaConnect
-            $dbcontroller = new bdTurmaConnect();
-            //Chamar o método para popular a variável código
-            $codigo = $dbcontroller->executeBuscaCodigoQuery($query);
-            
-            //Defnir as instruções SQL
+                $nome=$_POST['txtNome'];
+                $endereco=$_POST['txtEndereco'];
+                $fone=$_POST['txtPhone'];
+                $bairro=$_POST['txtBairro'];
+                $cidade=$_POST['txtCidade'];
+                $uf=$_POST['txtUF'];
+                $cep=$_POST['txtCEP'];
+                $email=$_POST['txtMail'];
+                
+                //Declarar a variável código como o último dado inserido na coluna
+                $query = "SELECT codContato from tbcontatos ORDER by codContato desc LIMIT 1";
+                //Instanciar a classe BdTurmaConnect
+                $dbcontroller = new bdTurmaConnect();
+                //Chamar o método para popular a variável código
+                $codigo = $dbcontroller->executeBuscaCodigoQuery($query);
+                
+                //Defnir as instruções SQL
 
-            $query = "INSERT INTO tbContatos (codContato,
-                                            nomedoContato,
-                                            enderecoContato,
-                                            telefoneContato,
-                                            bairro,
-                                            cidade,
-                                            uf,
-                                            cep,
-                                            emailContato)
+                $query = "INSERT INTO tbContatos (codContato,
+                                                nomedoContato,
+                                                enderecoContato,
+                                                telefoneContato,
+                                                bairro,
+                                                cidade,
+                                                uf,
+                                                cep,
+                                                emailContato)
 
-                                            VALUES ({$codigo},
-                                                    '{$nome}',
-                                                    '{$endereco}',
-                                                    '{$fone}',
-                                                    '{$bairro}',
-                                                    '{$cidade}',
-                                                    '{$uf}',
-                                                    '{$cep}',
-                                                    '{$email}')";
-            //Instanciar a classe BdTurmaConnect
-            $dbcontroller = new bdTurmaConnect();
-            //Chamar o método
-            $rawData = $dbcontroller->executeQuery($query);
-            //Verificar se o retorno está vazio
-            if(empty($rawData)){
-                $statusCode = 404;
-                $rawData = array('sucesso' => 0);
+                                                VALUES ({$codigo},
+                                                        '{$nome}',
+                                                        '{$endereco}',
+                                                        '{$fone}',
+                                                        '{$bairro}',
+                                                        '{$cidade}',
+                                                        '{$uf}',
+                                                        '{$cep}',
+                                                        '{$email}')";
+                //Instanciar a classe BdTurmaConnect
+                $dbcontroller = new bdTurmaConnect();
+                //Chamar o método
+                $rawData = $dbcontroller->executeQuery($query);
+                //Verificar se o retorno está vazio
+                if(empty($rawData)){
+                    $statusCode = 404;
+                    $rawData = array('sucesso' => 0);
+                }
+                else{
+                    $statusCode = 200;
+                    $rawData = array('sucesso' => 1);  
+                }
+                //Verificar qual o tipo de cabeçalho web
+                $requestContentType = $_POST['HTTP_ACCEPT'];
+                $this->setHttpHeaders($requestContentType,$statusCode);
+                $result["RetornoDados"] = $rawData;
+                //Verificar se o arquivo passado foi json
+                if(strpos($requestContentType,'application/json') !== false){
+                    $response = $this->encodeJson(($result));
+                    echo $response;
+                }
             }
-            else{
-                $statusCode = 200;
-                $rawData = array('sucesso' => 1);  
-            }
-            //Verificar qual o tipo de cabeçalho web
-            $requestContentType = $_POST['HTTP_ACCEPT'];
-            $this->setHttpHeaders($requestContentType,$statusCode);
-            $result["RetornoDados"] = $rawData;
-            //Verificar se o arquivo passado foi json
-            if(strpos($requestContentType,'application/json') !== false){
-                $response = $this->encodeJson(($result));
-                echo $response;
-            }
-        }
     }
 
     public function ContatosConsultar() {
-        if(isset($_POST['txtNome'])) {
+            if(isset($_POST['txtNome'])) {
 
-            $nome = $_POST['txtNome'];
-        
-            //Chamar o procedure de Conslta de contatos a partir da variável nome
-            $query = "CALL spConsultarContatos(:pnome)";
-            //Definir o conjunto de dados
-            $array = array(":pnome"=>"{$nome}");
-            //Instanciar a classe BdTurmaConnect
-            $dbcontroller = new bdTurmaConnect();
-            //Chamar o método execute procedure trazendo a partir das variáveis query e array
-            $rawData = $dbcontroller->executeProcedure($query,$array);
-            //Verificar se o retorno está vazio
-            if(empty($rawData)){
-                $statusCode = 404;
-                $rawData = array('sucesso' => 0);
+                $nome = $_POST['txtNome'];
+            
+                //Chamar o procedure de Conslta de contatos a partir da variável nome
+                $query = "CALL spConsultarContatos(:pnome)";
+                //Definir o conjunto de dados
+                $array = array(":pnome"=>"{$nome}");
+                //Instanciar a classe BdTurmaConnect
+                $dbcontroller = new bdTurmaConnect();
+                //Chamar o método execute procedure trazendo a partir das variáveis query e array
+                $rawData = $dbcontroller->executeProcedure($query,$array);
+                //Verificar se o retorno está vazio
+                if(empty($rawData)){
+                    $statusCode = 404;
+                    $rawData = array('sucesso' => 0);
+                }
+                else{
+                    $statusCode = 200;
+                }
+                //Verificar qual o tipo de cabeçalho web
+                $requestContentType = $_POST['HTTP_ACCEPT'];
+                $this->setHttpHeaders($requestContentType,$statusCode);
+                $result["RetornoDados"] = $rawData;
+                //Verificar se o arquivo passado foi json
+                if(strpos($requestContentType,'application/json') !== false){
+                    $response = $this->encodeJson(($result));
+                    echo $response;
+                }
             }
-            else{
-                $statusCode = 200;
-            }
-            //Verificar qual o tipo de cabeçalho web
-            $requestContentType = $_POST['HTTP_ACCEPT'];
-            $this->setHttpHeaders($requestContentType,$statusCode);
-            $result["RetornoDados"] = $rawData;
-            //Verificar se o arquivo passado foi json
-            if(strpos($requestContentType,'application/json') !== false){
-                $response = $this->encodeJson(($result));
-                echo $response;
-            }
-        }
     }
 
     public function encodeJson($responseData){
@@ -125,8 +125,17 @@ else{
     }
 }
 
-switch($page_key){
+if (isset($_POST['btnEnviar'])){
+    $page_key = "Incluir";
+    $_POST['HTTP_ACCEPT'] = "application/json";
+}
 
+if (isset($_POST['btnListar'])){
+    $page_key = "Consultar";
+    $_POST['HTTP_ACCEPT'] = "application/json"; 
+}
+
+switch($page_key){
     case "Consultar":
         $contatos = new ContatosRestHandler();
         $contatos-> ContatosConsultar();
@@ -136,4 +145,6 @@ switch($page_key){
         $contatos-> ContatosIncluir();
         break;
 }
+
+
 ?>
