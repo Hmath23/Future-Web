521dbf9616b43579286281d9c81567bbd84fe012
Matheus Henrique Caraça da Silva <hmatheus23@hotmail.com>
2022-06-08 22:25:28 -0300

    Finalização da configuração da tabela listar e criação do botão Buscar CEP

--- js/ValidaForm.js
+++ js/ValidaForm.js
@@ -31,6 +31,25 @@ $(document).ready(function(){
         // alert("Teste");
     ContatosConsultar();
     });
+	$('#btnEnviar').click(function(){
+    	ContatosIncluir();
+    });
+	$('#link_cep').click(function(){
+    	BuscaCep();
+    });
+});
+
+$(function(){
+	//Dialog
+	$('#dialog').dialog({
+		autoOpen: false,
+		width: 600,
+		buttons:{
+			"OK":function(){
+				$(this).dialog("close");
+			},
+		}
+	});
 });
 
 function ContatosConsultar(){
@@ -38,12 +57,10 @@ function ContatosConsultar(){
     var strnome = $('input[id=txtNome]').val();
 	//Definir a url
     var url = "../controllers/ControleContatos.php?page_key=Consultar"+"&txtNome="+strnome+"&HTTP_ACCEPT=application/json";
-
 	//Instanciar o método
 	var xmlreq = CriaRequest();
 	//Iniciar uma requisição
 	xmlreq.open('GET',url,true);
-
 	//Verificar a situação da conexão com o servidor
 	xmlreq.onreadystatechange = function(){
 		//Verificar se foi concluído com sucesso e se a conexão não foi fechada (readyState=4)
@@ -60,33 +77,63 @@ function ContatosConsultar(){
 	xmlreq.send(null);
 }
 
+function BuscaCep(){
+    var strcep = $('input[id=txtCEP]').val();
+    var url = "http://viacep.com.br/ws/"+strcep+"/json";
+	//Instanciar o método
+	var xmlreq = CriaRequest();
+	//Iniciar uma requisição
+	xmlreq.open('GET',url,true);
+	//Verificar a situação da conexão com o servidor
+	xmlreq.onreadystatechange = function(){
+		if(xmlreq.readyState == 4){
+			if(xmlreq.status == 200){
+				preencherCampos(JSON.parse(xmlreq.responseText));
+			}
+		}
+	};
+	xmlreq.send(null);
+}
+
 function MostrarContatos(obj){
-	var strTabela = "<table border=1><thead><th>Nome</th><th>Email</th><th>Telefone</th><th>CEP</th><th>Endereço</th><th>Bairro</th><th>Cidade</th><th>UF</th></thead>";
+	var strTabela = "<table border=1><thead><th>Nome</th><th>Email</th><th>Telefone</th><th>CEP</th><th>UF</th></thead>";
 	result = document.getElementById('Resultado');
 	if(obj.RetornoDados.length > 1){
 		for (var i=0;i < obj.RetornoDados.length;i++){
-			strTabela += "<tbody><tr><td>" +
+			strTabela += "<tbody><tr><td> " +
 			obj.RetornoDados[i].nomedoContato + '</td><td>' +
 			obj.RetornoDados[i].emailContato + '</td><td>' +
 			obj.RetornoDados[i].telefoneContato + '</td><td>' +
 			obj.RetornoDados[i].cep + '</td><td>' +
-			obj.RetornoDados[i].enderecoContato + '</td><td>' +
-			obj.RetornoDados[i].bairro + '</td><td>' +
-			obj.RetornoDados[i].cidade + '</td><td>' +
 			obj.RetornoDados[i].uf + '</td></tr>'
 		}
 		strTabela += "</tbody></table>";
 		result.innerHTML = strTabela;
+		$("#Listagem").modal();
 	}
 	else{
-		$('input[name=txtNome]').val(obj.RetornoDados[i].nomedoContato);
-		$('input[name=txtMail]').val(obj.RetornoDados[i].emailContato);
-		$('input[name=txtPhone]').val(obj.RetornoDados[i].telefoneContato);
-		$('input[name=txtCEP]').val(obj.RetornoDados[i].cep);
-		$('input[name=txtEndereco]').val(obj.RetornoDados[i].enderecoContato);
-		$('input[name=txtBairro]').val(obj.RetornoDados[i].bairro);
-		$('input[name=txtCidade]').val(obj.RetornoDados[i].cidade);
-		$('input[name=txtUF]').val(obj.RetornoDados[i].uf);
+		$('input[name=txtNome]').val(obj.RetornoDados[0].nomedoContato);
+		$('input[name=txtMail]').val(obj.RetornoDados[0].emailContato);
+		$('input[name=txtPhone]').val(obj.RetornoDados[0].telefoneContato);
+		$('input[name=txtCEP]').val(obj.RetornoDados[0].cep);
+		$('input[name=txtEndereco]').val(obj.RetornoDados[0].enderecoContato);
+		$('input[name=txtBairro]').val(obj.RetornoDados[0].bairro);
+		$('input[name=txtCidade]').val(obj.RetornoDados[0].cidade);
+		$('input[name=txtUF]').val(obj.RetornoDados[0].uf);
 	}
 }
 
+function preencherCampos(obj){
+	
+	if(obj.erro == true){
+		$("#Dialog").dialog('open');
+		$("#Mensagem").html('Cep Inválido');
+		$('input[id=txtCEP').val('');
+	}
+	else{
+		$('input[name=txtEndereco]').val(obj.logradouro);
+		$('input[name=txtBairro]').val(obj.bairro);
+		$('input[name=txtCidade]').val(obj.localidade);
+		$('input[name=txtUF]').val(obj.uf);
+	}
+}
